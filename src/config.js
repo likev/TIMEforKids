c48ad1a9f62f1baa66f2a7dd98ac0f37f4dbeb5b
likev <3146974+likev@users.noreply.github.com>
2023-02-23 11:48:12 +0000

    add home page info and css

--- src/config.js
+++ src/config.js
@@ -1,7 +1,8 @@
 
 const CONFIG = {
 
-    "CORS-PROXY": "https://cooperative-cuff-elk.cyclic.app" //"conversation",
+    "CORS-PROXY": "https://cooperative-cuff-elk.cyclic.app", //"conversation",
+    "TIMEforKids": "https://www.timeforkids.com" //"conversation",
 };
 
 export default CONFIG;
--- src/main.js
+++ src/main.js
@@ -12,19 +12,46 @@ import config from './config';
 //import { fixedEncodeURIComponent, getUTCTimeStr } from "./utils"
 
 async function get_article_lists() {
-    const f = await fetch("https://cooperative-cuff-elk.cyclic.app/https://www.timeforkids.com/g2/", {
-      //"mode": "cors"
+    const f = await fetch(`${config['CORS-PROXY']}/${config.TIMEforKids}/g2/`, {
+        //"mode": "cors"
     });
     const html = await f.text();
-    //console.log(html);
-    const main = $(html).find('#site > main').html();
-    //console.log( main )
-    return main;
+    console.log(html);
+    const main = $(html).find('main').html();
+    console.log(main)
+    return `<main>${main}</main>`;
 }
-  
-  
+
+async function fetchImage(url) {
+    let response = await fetch(url);
+    let blob = await response.blob();
+    let blobUrl = URL.createObjectURL(blob);
+    return blobUrl;
+}
+
 
 //document is ready
 $(async function () {
-    $('#main').html(await get_article_lists());
+    const mainHtml = await get_article_lists();
+    console.log(mainHtml);
+    const main = $(mainHtml);
+    main.find('a').each(function () {
+        let href = this.href;
+        this.href = href.replace("https://www.timeforkids.com/", `${config['CORS-PROXY']}/https://www.timeforkids.com/`);
+    })
+
+    main.find('img').each(function () {
+        let that = this;
+
+        async function set() {
+            let src = that.src;
+            src = src.replace("https://www.timeforkids.com/", `${config['CORS-PROXY']}/https://www.timeforkids.com/`);
+            that.src = await fetchImage(src);
+        }
+
+        set();
+
+    })
+
+    $('#main').html(main);
 })
